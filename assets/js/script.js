e4e05f9d19c77d104719a872e14d83b4e93b5dac
giomendes.quezada <gioq89@gmail.com>
2023-05-13 16:56:17 -0700

    added js to search history

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,17 @@
 // API key
-var apiKey = '';
+var apiKey = "";
 var apiURL =
-  'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}';
+  "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
 
 // DOM Elements
-var cityNameInput = document.getElementById('city-name');
-var searchForm = document.querySelector('form');
-var currentWeather = document.getElementById('current-day');
-var forecastSection = document.getElementById('forecast');
-var searchHistory = document.getElementById('search-history');
+var cityNameInput = document.getElementById("city-name");
+var searchForm = document.querySelector("form");
+var currentWeather = document.getElementById("current-day");
+var forecastSection = document.getElementById("forecast");
+var searchHistory = document.getElementById("search-history");
 
 // Event Listener
-searchForm.addEventListener('submit', function (event) {
+searchForm.addEventListener("submit", function (event) {
   event.preventDefault();
   const cityName = cityNameInput.value.trim();
   if (cityName) {
@@ -24,9 +24,9 @@ searchForm.addEventListener('submit', function (event) {
 function getCurrentDayForcast(city) {
   var apiKey = getApiKey();
   var apiUrl =
-    'https://api.openweathermap.org/data/2.5/weather?q=' +
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
     city +
-    '&units=metric&appid=' +
+    "&units=metric&appid=" +
     apiKey;
 
   fetch(apiUrl)
@@ -34,23 +34,24 @@ function getCurrentDayForcast(city) {
       if (response.ok) {
         return response.json();
       }
-      throw new Error('Network response was not ok.');
+      throw new Error("Network response was not ok.");
     })
     .then(function (data) {
       showCurrentWeather(data);
+      addToSearchHistory(data.name);
     })
     .catch(function (error) {
-      console.error('There was a problem fetching weather data: ', error);
-      showError('There was a problem fetching weather data. Please try again.');
+      console.error("There was a problem fetching weather data: ", error);
+      showError("There was a problem fetching weather data. Please try again.");
     });
 }
 
 function getFiveDayForecast(city) {
   var apiKey = getApiKey();
   var apiUrl =
-    'https://api.openweathermap.org/data/2.5/forecast?q=' +
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
     city +
-    '&units=metric&appid=' +
+    "&units=metric&appid=" +
     apiKey;
 
   fetch(apiUrl)
@@ -58,22 +59,22 @@ function getFiveDayForecast(city) {
       if (response.ok) {
         return response.json();
       }
-      throw new Error('Network response was not ok.');
+      throw new Error("Network response was not ok.");
     })
     .then(function (data) {
       showFiveDayForecast(data);
     })
     .catch(function (error) {
-      console.error('There was a problem fetching weather data: ', error);
-      showError('There was a problem fetching weather data. Please try again.');
+      console.error("There was a problem fetching weather data: ", error);
+      showError("There was a problem fetching weather data. Please try again.");
     });
 }
 
 // function to retrieve API for searchWeather
 function getApiKey() {
-  var apiKey = localStorage.getItem('openWeatherMapAPIKey');
+  var apiKey = localStorage.getItem("openWeatherMapAPIKey");
   if (!apiKey) {
-    apiKey = 'api needed';
+    apiKey = "api needed";
   }
 
   return apiKey;
@@ -82,8 +83,8 @@ function getApiKey() {
 //   function to retrive specific info for searchWeather function
 function showCurrentWeather(list) {
   var cityNameInput = list.name;
-  var currentDate = dayjs().format('MM/DD/YY');
-  $('#current-day').text(currentDate);
+  var currentDate = dayjs().format("MM/DD/YY");
+  $("#current-day").text(currentDate);
   var weatherIcon = list.weather[0].icon;
   var tempCelsius = list.main.temp;
   var tempFahrenheit = (tempCelsius * 9) / 5 + 32;
@@ -107,18 +108,18 @@ function showCurrentWeather(list) {
 }
 
 function showFiveDayForecast(list) {
-  var forecastHTML = '';
+  var forecastHTML = "";
 
   for (var i = 0; i < list.list.length; i++) {
     var forecast = list.list[i];
-    var date = forecast.dt_txt.split(' ')[0];
-    var time = forecast.dt_txt.split(' ')[1];
-    var hour = time.split(':')[0];
+    var date = forecast.dt_txt.split(" ")[0];
+    var time = forecast.dt_txt.split(" ")[1];
+    var hour = time.split(":")[0];
     var temp = forecast.main.temp;
     var humidity = forecast.main.humidity;
     var weatherIcon = forecast.weather[0].icon;
 
-    if (time === '15:00:00') {
+    if (time === "15:00:00") {
       forecastHTML += `
           <div class="card">
             <div class="card-body">
@@ -134,3 +135,39 @@ function showFiveDayForecast(list) {
 
   forecastSection.innerHTML = forecastHTML;
 }
+
+function addToSearchHistory(cityName) {
+  var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
+  cityName = cityName.trim();
+  if (cityName) {
+    if (searchHistory.includes(cityName)) {
+      var index = searchHistory.indexOf(cityName);
+      searchHistory.splice(index, 1);
+    }
+
+    if (searchHistory.length > 10) {
+      searchHistory.pop();
+    }
+    
+    searchHistory.unshift(cityName);
+
+    localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+    displaySearchHistory(searchHistory);
+  }
+}
+
+function displaySearchHistory(searchHistory) {
+  var searchHistoryList = document.getElementById("search-history-list");
+  searchHistoryList.innerHTML = "";
+
+  for (var i = 0; i < searchHistory.length; i++) {
+    var listItem = document.createElement("li");
+    listItem.textContent = searchHistory[i];
+    searchHistoryList.appendChild(listItem);
+  }
+}
+
+window.addEventListener("load", function () {
+  var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
+  displaySearchHistory(searchHistory);
+});
